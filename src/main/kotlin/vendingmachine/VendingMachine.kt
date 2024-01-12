af5c6fce06e19e4ff87e4bf04bc8b6df2b6ca9ad
Miguel Ángel Pérez García <miguel.perez@tii.ae>
2024-01-12 13:17:10 +0000

    green-test: display nickels amount when a nickel coin is inserted

--- src/main/kotlin/vendingmachine/VendingMachine.kt
+++ src/main/kotlin/vendingmachine/VendingMachine.kt
@@ -1,9 +1,9 @@
 package vendingmachine
 
 class VendingMachine {
+    var display: String = "INSERT COIN"
+
     fun insertCoin(d: Double) {
-
+        display = "$0.05"
     }
-
-    val display: String = "INSERT COIN"
 }
