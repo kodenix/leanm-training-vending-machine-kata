98a73b65d1e1e6d504f0a5c35e0ab157b1880c74
Alberto Morales <kodenix@gmail.com>
2024-01-12 15:07:53 +0100

    green-test: display updated amount when a nickel coin is inserted and there is already a coin inserted

--- src/main/kotlin/vendingmachine/VendingMachine.kt
+++ src/main/kotlin/vendingmachine/VendingMachine.kt
@@ -1,21 +1,28 @@
 package vendingmachine
 
+import kotlin.math.roundToLong
+
 class VendingMachine {
+    private var currentAmount: Double = 0.0
     var display: String = "INSERT COIN"
 
+
     fun insertCoin(d: Double) {
-        display = when (d) {
+        when (d) {
             0.10 -> {
-                "$0.10"
+                currentAmount =0.10
             }
 
             0.25 -> {
-                "$0.25"
+                currentAmount =0.25
             }
 
             else -> {
-                "$0.05"
+                currentAmount +=0.05
             }
         }
+
+        display = "$" + "%.2f".format(currentAmount)
     }
+
 }
