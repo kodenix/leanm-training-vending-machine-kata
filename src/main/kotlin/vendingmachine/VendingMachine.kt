01e534965de5239d43b4e9fd856937af560a71ee
Miguel Ángel Pérez García <miguel.perez@tii.ae>
2024-01-12 13:31:32 +0000

    ref: replace insert coin if cascade to when statement

--- src/main/kotlin/vendingmachine/VendingMachine.kt
+++ src/main/kotlin/vendingmachine/VendingMachine.kt
@@ -4,12 +4,18 @@ class VendingMachine {
     var display: String = "INSERT COIN"
 
     fun insertCoin(d: Double) {
-        display = if (d == 0.10) {
-            "$0.10"
-        } else if (d == 0.25) {
-            "$0.25"
-        } else {
-            "$0.05"
+        display = when (d) {
+            0.10 -> {
+                "$0.10"
+            }
+
+            0.25 -> {
+                "$0.25"
+            }
+
+            else -> {
+                "$0.05"
+            }
         }
     }
 }
