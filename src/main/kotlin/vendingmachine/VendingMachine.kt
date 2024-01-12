4a55a96bb1f0e2da56ea70a0a38628271f8719d7
Miguel Ángel Pérez García <miguel.perez@tii.ae>
2024-01-12 13:30:36 +0000

    green-test: display quarter amount when a quarter coin is inserted

--- src/main/kotlin/vendingmachine/VendingMachine.kt
+++ src/main/kotlin/vendingmachine/VendingMachine.kt
@@ -6,6 +6,8 @@ class VendingMachine {
     fun insertCoin(d: Double) {
         display = if (d == 0.10) {
             "$0.10"
+        } else if (d == 0.25) {
+            "$0.25"
         } else {
             "$0.05"
         }
