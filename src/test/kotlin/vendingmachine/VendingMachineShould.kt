b18861a477f9f674ec85828be34d2e0de9fade0c
Alberto Morales <kodenix@gmail.com>
2024-01-12 21:10:58 +0100

    red-test: display updated amount when a dime coin is inserted and there is already a coin inserted

--- src/test/kotlin/vendingmachine/VendingMachineShould.kt
+++ src/test/kotlin/vendingmachine/VendingMachineShould.kt
@@ -31,12 +31,30 @@ class VendingMachineShould {
     }
 
     @Test
+    fun `display updated amount when a nickel coin is inserted and there is already two coin inserted`() {
+        val vendingMachine = VendingMachine()
+
+        vendingMachine.insertCoin(0.10)
+        vendingMachine.insertCoin(0.05)
+
+        assertEquals("$0.15", vendingMachine.display)
+    }
+
+    @Test
     fun `display dimes amount when a dime coin is inserted`() {
         val vendingMachine = VendingMachine()
 
         vendingMachine.insertCoin(0.10)
 
         assertEquals("$0.10", vendingMachine.display)
+    }
+
+    @Test
+    fun `display updated amount when a dime coin is inserted and there is already two coin inserted`() {
+        val vendingMachine = VendingMachine()
+        vendingMachine.insertCoin(0.10)
+        vendingMachine.insertCoin(0.10)
+        assertEquals("$0.20", vendingMachine.display)
     }
 
     @Test
